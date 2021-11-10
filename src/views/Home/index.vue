82e46beb571905cfdd4e93596266047ddfee24f0
zl <zl.@xx.com>
2021-11-10 16:48:56 +0800

    feat: 目录调整

---
+++ src/views/Home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">Home</div>
+</template>
+
+<script>
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    setup() {
+
+    },
+  });
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    display: flex;
+  }
+</style>
