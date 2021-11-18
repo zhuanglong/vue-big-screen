600da764f77296df012dfb0383c777506ee464d1
zl <zl.@xx.com>
2021-11-18 14:42:58 +0800

    feat: 流量卡片

---
+++ src/views/Home/Section3/FlowRate/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="chart-FlowRate" :style="{ width, height }">
+    <img class="img" :src="getImageUrl(data.id)" alt="" />
+    <div class="content">
+      <span class="desc">{{data.desc}}</span>
+      <span class="count-wrapper">
+        <span class="number">{{data.value}}</span>
+        <span class="unit">{{data.unit}}</span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+
+  const props = defineProps({
+    data: {
+      type: Object,
+      default: () => ({}),
+    },
+    width: {
+      type: String,
+      default: '300px',
+    },
+    height: {
+      type: String,
+      default: '150px',
+    },
+  });
+
+  const getImageUrl = (id) => {
+    return new URL(`../../../../assets/images/center-details-data${id}.png`, import.meta.url).href;
+  };
+</script>
+
+<style lang="scss" scoped>
+  .chart-FlowRate {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0 10px;
+    border-radius: 5px;
+    border: 1px solid #343f4b;
+    background-color: rgba(19,25,47,0.8);
+
+    .img {
+      width: 100px;
+      height: 100px;
+      margin-right: 20px;
+    }
+
+    .content {
+      display: flex;
+      flex-direction: column;
+
+      .desc {
+        font-size: 16px;
+        color: #bcdcff;
+      }
+
+      .count-wrapper {
+        display: flex;
+        flex-direction: row;
+        align-items: baseline;
+        margin-top: 20px;
+
+        .number {
+          font-size: 24px;
+          color: transparent;
+          font-weight: bolder;
+          background: linear-gradient(to bottom,#fff,#4db6e5);
+          -webkit-background-clip: text;
+          background-clip: text;
+        }
+
+        .unit {
+          margin-left: 10px;
+          font-size: 18px;
+          font-weight: bold;
+          color: transparent;
+          background: linear-gradient(to bottom,#fff,#4db6e5);
+          -webkit-background-clip: text;
+          background-clip: text;
+        }
+      }
+    }
+  }
+</style>
--- src/views/Home/Section3/index.vue
+++ src/views/Home/Section3/index.vue
@@ -1,19 +1,77 @@
 <template>
   <div class="section3-container">
-    <Map />
+    <Map width="100%" height="93%" />
+    <div class="chat-group">
+      <FlowRate
+        v-for="item in flowRateData"
+        :key="item.id"
+        :data="item"
+        width="100%"
+        height="100%"
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
   import { ref } from 'vue';
   import Map from './Map/index.vue';
+  import FlowRate from './FlowRate/index.vue';
+
+  const flowRateData = ref([
+    {
+      id: 1,
+      desc: '图像转化流量',
+      value: 260,
+      unit: '点击/张',
+    },
+    {
+      id: 2,
+      desc: '视频转化流量',
+      value: 330,
+      unit: '点击/个',
+    },
+    {
+      id: 3,
+      desc: '公众号流量',
+      value: 62356,
+      unit: '点击/篇',
+    },
+    {
+      id: 4,
+      desc: '安全服务流量',
+      value: 5453,
+      unit: '攻击/小时',
+    },
+    {
+      id: 5,
+      desc: '金融流量',
+      value: 4.65,
+      unit: '万/小时',
+    },
+    {
+      id: 6,
+      desc: '通信流量',
+      value: 1250,
+      unit: 'M /分',
+    },
+  ]);
 </script>
 
 <style lang="scss" scoped>
   .section3-container {
     display: grid;
-    grid-template-columns: 980px;
-    grid-template-rows: 70% 30%;
-    margin: 0 10px;
+    grid-template-columns: 960px;
+    grid-template-rows: 710px 300px;
+    padding: 0 20px;
+
+    .chat-group {
+      display: grid;
+      grid-template-columns: repeat(3, 313px);
+      grid-template-rows: repeat(2, 145px);
+      column-gap: 10px;
+      row-gap: 10px;
+      justify-content: center;
+    }
   }
 </style>
